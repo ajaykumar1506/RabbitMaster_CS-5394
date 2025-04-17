a99215fd7111cf3ae438468b99cd3603684c2c75
ybk20 <ybk20@txstate.edu>
2025-04-17 14:44:36 -0500

    Added products file

---
+++ products/services/BaseURLs.js
@@ -0,0 +1,2 @@
+export const PRODUCTS_BASEURL = 'https://rabbitmart-ell1-887hg4751-shyams-projects-78ac3144.vercel.app/products'
+export const USER_BASEURL = 'https://rabbitmart-ell1-887hg4751-shyams-projects-78ac3144.vercel.app/me'
